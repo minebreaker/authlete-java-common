e7c4e3ac19dca469e58dbb25edc7e2047c18f156
tkoyama <tkoyama@folio-sec.com>
2021-10-04 22:24:17 +0900

    added mv to build script

--- build-native.sh
+++ build-native.sh
@@ -1,4 +1,10 @@
-native-image -jar target/authlete-java-common-3.1-SNAPSHOT-jar-with-dependencies.jar \
+#!/bin/bash
+
+
+export VERSION="3.1-SNAPSHOT"
+native-image -jar "target/authlete-java-common-${VERSION}-jar-with-dependencies.jar" \
   --allow-incomplete-classpath \
   --enable-url-protocols=https \
   -H:ReflectionConfigurationFiles=reflect-config.json,authlete-reflect-config.json
+mv "authlete-java-common-${VERSION}-jar-with-dependencies" authlete
+
